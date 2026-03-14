b69dd06e7528c8a306f4e7552bc7c1c42f3660da
Manthan B Y <manthan.gowda@gmail.com>
2026-03-14 22:42:29 +0530

    feat: implement host-based routing for life.manthanby.cv

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "life.manthanby.cv",
+                    },
+                ],
+                destination: "/admin/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
